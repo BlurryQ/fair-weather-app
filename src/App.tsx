98ff39f97947d41bcfa4b934a75b3f98bd41759a
BlurryQ <jazz.barlow@outlook.com>
2024-11-17 21:02:37 +0000

    Added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import Header from './assets/components/header'
+import Sunrise from './assets/components/Sunrise'
+import Sunset from './assets/components/Sunset'
+import CurrentCondition from './assets/components/CurrentCondition'
+
+
+export type weatherData = {
+  text: string,
+  icon: string,
+}
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState<string>('')
-  const [currentWeatherIcon, setCurrentWeatherIcon] = useState<string>('')
+  const [sunriseTime, setSunriseTime] = useState<string>('loading')
+  const [sunsetTime, setSunsetTime] = useState<string>('loading')
+  const [weatherData, setWeatherData] = useState<weatherData | object>({})
 
   useEffect(() => {
-    const api_key = import.meta.env.VITE_API_KEY;
+    const api_key: string = import.meta.env.VITE_API_KEY;
     axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=BH119FS&days=3&aqi=no&alerts=yes`)
       .then((data) => {
-        // console.log(data.status)
         // console.log(data.data)
-        const weather: string = data.data.current.condition.text
-        const iconData: string = data.data.current.condition.icon
-        const iconURL: string = iconData.split("").slice(2,).join("")
-        setCurrentWeather(weather)
-        setCurrentWeatherIcon(`https://${iconURL}`)
+        const sunrise: string = data.data.forecast.forecastday[0].astro.sunrise
+        const sunset: string = data.data.forecast.forecastday[0].astro.sunset
+        setSunriseTime(sunrise)
+        setSunsetTime(sunset)
+        setWeatherData(data.data.current.condition)
       })
       .catch((err) => {
         console.log('Code:', err.response.data.error.code)
@@ -26,9 +36,10 @@ function App() {
 
   return (
     <>
-      <h1>Fair Weather App</h1>
-      {currentWeatherIcon ? <img src={currentWeatherIcon}></img> : null}
-      <p>{currentWeather}</p>
+      <Header />
+      <Sunrise time={sunriseTime} />
+      <CurrentCondition weatherData={weatherData} />
+      <Sunset time={sunsetTime} />
     </>
   )
 }
