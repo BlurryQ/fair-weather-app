aec0d2502e9d40b353463c7ea77496222e49b6aa
BlurryQ <jazz.barlow@outlook.com>
2024-11-18 17:05:38 +0000

    Adds Location component

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,4 +1,10 @@
+import '../../styles/header.css'
+import Location from './Location'
+
 export default function Header(): JSX.Element {
 
-    return <h1>Fair Weather App</h1>
+    return <>
+        <h1 className='header'>Fair Weather App</h1>
+        <Location />
+    </>
 }
