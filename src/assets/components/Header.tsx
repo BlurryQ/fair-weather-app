97025a59284a534bb701775a916fce1fa6cc3f1f
BlurryQ <jazz.barlow@outlook.com>
2024-11-20 19:34:58 +0000

    Removes Location component

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,10 +1,6 @@
 import '../../styles/header.css'
-import Location from './Location'
 
 export default function Header(): JSX.Element {
 
-    return <>
-        <h1 className='header'>Fair Weather App</h1>
-        <Location />
-    </>
+    return <h1 className='header'>Fair Weather App</h1>
 }
