b4d4bb00e3a2ddf746df10c2c762081c92fb5151
BlurryQ <jazz.barlow@outlook.com>
2024-12-08 20:30:55 +0000

    Improves readability

--- src/assets/components/LocationList.tsx
+++ src/assets/components/LocationList.tsx
@@ -3,10 +3,8 @@ import "../../styles/location.css";
 // types
 import { Autocomplete } from "../../types/Autocomplete";
 type LocationListProps = {
-  autocomplete: Autocomplete | Autocomplete[];
-  setAutocomplete: React.Dispatch<
-    React.SetStateAction<Autocomplete | Autocomplete[] | null>
-  >;
+  autocomplete: Autocomplete[];
+  setAutocomplete: React.Dispatch<React.SetStateAction<Autocomplete[] | null>>;
   setLocation: React.Dispatch<React.SetStateAction<string>>;
   setLongitude: React.Dispatch<React.SetStateAction<number>>;
   setLatitude: React.Dispatch<React.SetStateAction<number>>;
@@ -30,37 +28,19 @@ export default function LocationList({
     setLocation(location);
   };
 
-  //   console.log(autocomplete.length);
-  //   console.log(autocomplete);
-
-  //   if (autocomplete.length === 1) {
-  //     return (
-  //       <li
-  //         key={autocomplete[0].id}
-  //         value={`${autocomplete[0].lat} ${autocomplete[0].lon}`}
-  //         onClick={selectLocation}
-  //       >
-  //         {autocomplete[0].name}, {autocomplete[0].region}
-  //       </li>
-  //     );
-  //   } else if (autocomplete.length >= 2) {
-  //     autocomplete.map((location: Autocomplete) => {
-  //       return (
-  //         <li
-  //           key={location.id}
-  //           value={`${location.lat} ${location.lon}`}
-  //           onClick={selectLocation}
-  //         >
-  //           {location.name}, {location.region}
-  //         </li>
-  //       );
-  //     });
-  //   } else return null;
-  // }
-
   return (
     <>
-      {autocomplete.length > 1 ? (
+      {/* if only one location returned, display it */}
+      {autocomplete.length === 1 ? (
+        <li
+          key={autocomplete[0].id}
+          value={`${autocomplete[0].lat} ${autocomplete[0].lon}`}
+          onClick={selectLocation}
+        >
+          {autocomplete[0].name}, {autocomplete[0].region}
+        </li>
+      ) : (
+        // else loop through and display all matching results
         autocomplete.map((location: Autocomplete) => {
           return (
             <li
@@ -72,14 +52,6 @@ export default function LocationList({
             </li>
           );
         })
-      ) : (
-        <li
-          key={autocomplete[0].id}
-          value={`${autocomplete[0].lat} ${autocomplete[0].lon}`}
-          onClick={selectLocation}
-        >
-          {autocomplete[0].name}, {autocomplete[0].region}
-        </li>
       )}
     </>
   );
