f2bfa349ac9f27050eef6886fd3da4a7352e490e
BlurryQ <jazz.barlow@outlook.com>
2025-01-04 15:18:40 +0000

    Renames function for generalised useage

--- src/assets/components/HourlyWeather.tsx
+++ src/assets/components/HourlyWeather.tsx
@@ -2,15 +2,18 @@ import Forecast from "./Forecast"
 import { HourProp } from "../../types/HourProp"
 import { HoursOverview } from "../../types/HoursOverview"
 
-export default function TodaysWeather({ todaysHours }: { todaysHours: HoursOverview}): JSX.Element {
+export default function HourlyWeather({ hours }: { hours: HoursOverview}): JSX.Element {
 
-    return <div className="todays-weather">
+    return <>
+            <div className="divide"></div>
+            <div className="todays-weather">
                 {
-                    todaysHours.map((hour: HourProp) => {
+                    hours.map((hour: HourProp) => {
                         return <div key={hour.time_epoch}>
                             <Forecast hour={hour} />
                         </div>
                         })
                 }
         </div>
+        </>
 }
