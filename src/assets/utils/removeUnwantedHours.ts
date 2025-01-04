b620d7d24d01363fb7876ffda60e5077651207a7
BlurryQ <jazz.barlow@outlook.com>
2025-01-04 15:13:36 +0000

    Adds ability to deal with multiple days

--- src/assets/utils/removeUnwantedHours.ts
+++ src/assets/utils/removeUnwantedHours.ts
@@ -1,13 +1,23 @@
 import { HoursOverview } from "../../types/HoursOverview";
 import { HourProp } from "../../types/HourProp";
+import { format } from "date-fns"
 
-export default function removeUnwantedHours(todaysHours : HoursOverview): HoursOverview {
-    const relevantHours: HoursOverview =  todaysHours.filter((hour: HourProp) => {
-        const today: Date = new Date()
-        const currentHour: number = today.getHours()
-        const epochDate: Date = new Date(hour.time_epoch * 1000)
-        const epochHour: number = epochDate.getHours()
-        if (currentHour > epochHour || epochHour < 9 || epochHour > 22) return false
+export default function removeUnwantedHours(hours: HoursOverview): HoursOverview {
+    const earliestTime: number = 9
+    const latestTime: number = 22
+
+    const [hoursDate]: string[] = hours[0].time.split(" ")
+
+    const today: Date = new Date()
+    const todayFormattedDate = format(today, "yyyy-MM-dd")
+    const currentHour: number = today.getHours()
+
+    const relevantHours: HoursOverview = hours.filter((hour: HourProp) => {
+        const epochHour: number = new Date(hour.time_epoch * 1000).getHours()
+        const outsideRelevantHours: boolean = epochHour < earliestTime || epochHour > latestTime
+        const isToday: boolean = todayFormattedDate === hoursDate
+
+        if ((isToday && currentHour > epochHour) || outsideRelevantHours) return false
         return true
     })
     return relevantHours
