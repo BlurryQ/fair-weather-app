ebd24bc5f66d286c374a02695db6efddad1c4141
BlurryQ <jazz.barlow@outlook.com>
2025-01-05 12:38:28 +0000

    Adds prop to be implemented (greyBG)

--- src/assets/components/HourlyWeather.tsx
+++ src/assets/components/HourlyWeather.tsx
@@ -2,11 +2,12 @@ import Forecast from "./Forecast"
 import { HourProp } from "../../types/HourProp"
 import { HoursOverview } from "../../types/HoursOverview"
 
-export default function HourlyWeather({ hours }: { hours: HoursOverview}): JSX.Element {
+export default function HourlyWeather({ hours }: { hours: HoursOverview}, {greyBG} : {greyBG: boolean}): JSX.Element {
 
+    // TODO add greyBG to CSS and enabked beneath
     return <>
             <div className="divide"></div>
-            <div className="todays-weather">
+            <div className="weather ">
                 {
                     hours.map((hour: HourProp) => {
                         return <div key={hour.time_epoch}>
