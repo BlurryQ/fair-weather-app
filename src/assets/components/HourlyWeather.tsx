36496509bc6f379c9d01eda8b1e3cc391bc965f7
BlurryQ <jazz.barlow@outlook.com>
2025-01-15 17:45:33 +0000

    Removes no longer required element

--- src/assets/components/HourlyWeather.tsx
+++ src/assets/components/HourlyWeather.tsx
@@ -1,27 +1,27 @@
-import Forecast from "./Forecast"
-import { HourProp } from "../types/HourProp"
-import { HoursOverview } from "../types/HoursOverview"
+import Forecast from "./Forecast";
+import { HourProp } from "../types/HourProp";
+import { HoursOverview } from "../types/HoursOverview";
 
 type HourlyWeatherProps = {
-    hours: HoursOverview;
-    tommorowBG: boolean;
-  };
-  
-  export default function HourlyWeather({
-    hours,
-    tommorowBG,
-  }: HourlyWeatherProps): JSX.Element {
+  hours: HoursOverview;
+  tommorowBG: boolean;
+};
 
-    return <>
-            <div className="divide"></div>
-            <div className="weather">
-                {
-                    hours.map((hour: HourProp) => {
-                        return <div key={hour.time_epoch}>
-                            <Forecast hour={hour} tommorowBG={tommorowBG}/>
-                        </div>
-                        })
-                }
-        </div>
-        </>
+export default function HourlyWeather({
+  hours,
+  tommorowBG,
+}: HourlyWeatherProps): JSX.Element {
+  return (
+    <>
+      <div className="weather">
+        {hours.map((hour: HourProp) => {
+          return (
+            <div key={hour.time_epoch}>
+              <Forecast hour={hour} tommorowBG={tommorowBG} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
