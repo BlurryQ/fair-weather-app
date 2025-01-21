3498946689a30a116fc8466c91395fcf46a05db1
BlurryQ <jazz.barlow@outlook.com>
2025-01-21 20:38:54 +0000

    Rearranges images

--- src/assets/utils/getImages.ts
+++ src/assets/utils/getImages.ts
@@ -1,28 +1,34 @@
 import { HourProp } from "../types/HourProp";
 
-import burntDobe from "/images/burn.png"
+import burntDobe from "/images/burnt.png"
 import coldDobe from "/images/cold.png"
+import foggyDobe from "/images/foggy.png"
 import hurricane from "/images/hurricane.png"
 import jacketDobe from "/images/jacket.png"
-import normalDobe from "/images/normal.png"
-import rainyDobe from "/images/rain.png"
+import rainyDobe from "/images/rainy.png"
+import snowyDobe from "/images/snowy.png"
 import sunnyDobe from "/images/sunny.png"
 import windy from "/images/windy.png"
+import placeholder from "/favicon.png"
 
 export default function getImages(weather: HourProp): string[] {
     const images = []
-    if (weather.uv > 3) images.push(burntDobe)
-
-    if (weather.will_it_rain || weather.chance_of_rain >= 60) images.push(rainyDobe)
-
-    if (weather.temp_c > 15) images.push(sunnyDobe)
+    
+    if (weather.condition.text.includes("snow")) images.push(snowyDobe)
+        else if (weather.will_it_rain || weather.chance_of_rain >= 60) images.push(rainyDobe)
+    
+    if (weather.temp_c > 18) images.push(sunnyDobe)
     else if (weather.temp_c < 5) images.push(coldDobe)
     else images.push(jacketDobe)
+
+    if (weather.uv > 3) images.push(burntDobe)
 
     if (weather.wind_mph >= 35) images.push(hurricane)
     else if (weather.wind_mph >= 20) images.push(windy)
 
-    if (images.length < 1) images.push(normalDobe)
+    if (weather.vis_miles <= 2) images.push(foggyDobe)
+
+    while (images.length < 4) images.push(placeholder)
 
     return images
 }
