27bcef8a0ed8a316bc8a1a952cd4efa159656970
BlurryQ <jazz.barlow@outlook.com>
2025-01-22 16:48:48 +0000

    Adds react setters

--- src/assets/models/weatherModel.ts
+++ src/assets/models/weatherModel.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { Autocomplete } from "../types/Autocomplete";
 import { WeatherDataProp } from "../types/WeatherDataProp";
 
-export function getAutocompleteWeather(setData: React.Dispatch<React.SetStateAction<Autocomplete[] | null>>, query: string) {
+export function getAutocompleteWeather(
+    setData: React.Dispatch<React.SetStateAction<Autocomplete[] | null>>, 
+    query: string,   
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>) {
 
     const api_key: string = import.meta.env.VITE_API_KEY;
     const url: string = `https://api.weatherapi.com/v1/search.json?key=${api_key}&q=${query}`;
@@ -10,14 +13,19 @@ export function getAutocompleteWeather(setData: React.Dispatch<React.SetStateAct
         .get(url)
         .then((data) => {
             if (setData) setData(data.data);
+            setLoading(false)
         })
-        .catch((err) => console.error(err));
+        .catch((err) => {
+            setLoading(false)
+            console.error(err)
+        });
 }
 
 export function getLatandLongWeather(latitude: number,
     longitude: number,
     setData: React.Dispatch<React.SetStateAction<WeatherDataProp | null>>,
-    setLocation: React.Dispatch<React.SetStateAction<string>>) {
+    setLocation: React.Dispatch<React.SetStateAction<string>>,
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>) {
 
     const api_key: string = import.meta.env.VITE_API_KEY;
     const url: string = `https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${latitude},${longitude}&days=3`;
@@ -27,7 +35,12 @@ export function getLatandLongWeather(latitude: number,
             setData(data.data);
             const townAndRegion: string = `${data.data.location.name}, ${data.data.location.region}`;
             setLocation(townAndRegion);
+            setLoading(false)
         })
-        .catch((err) => console.error(err));
+        .catch((err) => {
+            setLoading(false)
+            console.error(err)
+        });
+        
 }
 
