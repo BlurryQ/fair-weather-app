b684bc7b897689849b8477a93eca79f84a9bccfa
BlurryQ <jazz.barlow@outlook.com>
2025-01-22 20:28:21 +0000

    Displays chosen hours

--- src/assets/components/HourlyWeather.tsx
+++ src/assets/components/HourlyWeather.tsx
@@ -1,19 +1,34 @@
-import Forecast from "./Forecast";
-import { HourProp } from "../types/HourProp";
-import { HoursOverview } from "../types/HoursOverview";
+import Forecast from './Forecast';
+import { HourProp } from '../types/HourProp';
+import { HoursOverview } from '../types/HoursOverview';
+import { useState } from 'react';
 
 export default function HourlyWeather({
   hours,
 }: {
   hours: HoursOverview;
 }): JSX.Element {
+  const [chosenIndex, setChosenIndex] = useState<number>(1);
+  console.log(hours);
+
+  const arrToUse: HourProp[] = [
+    hours[chosenIndex - 1],
+    hours[chosenIndex],
+    hours[chosenIndex + 1],
+  ];
+
   return (
     <>
       <div className="weather">
-        {hours.map((hour: HourProp) => {
+        {arrToUse.map((hour: HourProp, index: number) => {
           return (
             <div key={hour.time_epoch}>
-              <Forecast hour={hour} />
+              <Forecast
+                hour={hour}
+                index={index}
+                chosenIndex={chosenIndex}
+                setChosenIndex={setChosenIndex}
+              />
             </div>
           );
         })}
