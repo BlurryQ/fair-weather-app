7181485c0c713e9d7e59e7cb1ecb7a9e15721f5d
BlurryQ <jazz.barlow@outlook.com>
2025-01-22 16:49:40 +0000

    Adds prop for selected card

--- src/assets/components/HourlyWeather.tsx
+++ src/assets/components/HourlyWeather.tsx
@@ -1,19 +1,26 @@
-import Forecast from "./Forecast";
-import { HourProp } from "../types/HourProp";
-import { HoursOverview } from "../types/HoursOverview";
+import Forecast from './Forecast';
+import { HourProp } from '../types/HourProp';
+import { HoursOverview } from '../types/HoursOverview';
+import { useState } from 'react';
 
 export default function HourlyWeather({
   hours,
 }: {
   hours: HoursOverview;
 }): JSX.Element {
+  const [selectedIndex, setSelectedIndex] = useState<number>(1);
   return (
     <>
       <div className="weather">
-        {hours.map((hour: HourProp) => {
+        {hours.map((hour: HourProp, index: number) => {
           return (
-            <div key={hour.time_epoch}>
-              <Forecast hour={hour} />
+            <div data-weather-card={hour.time_epoch} key={hour.time_epoch}>
+              <Forecast
+                hour={hour}
+                index={index}
+                selectedIndex={selectedIndex}
+                setSelectedIndex={setSelectedIndex}
+              />
             </div>
           );
         })}
