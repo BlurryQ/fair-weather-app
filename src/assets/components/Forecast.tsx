ba4ffc165d0c718a10e289cba64a61fb5d3014b6
BlurryQ <jazz.barlow@outlook.com>
2025-01-22 20:03:15 +0000

    Implements component

--- src/assets/components/Forecast.tsx
+++ src/assets/components/Forecast.tsx
@@ -5,6 +5,7 @@ import { HourProp } from '../types/HourProp';
 import getImages from '../utils/getImages';
 import WeatherTable from './WeatherTable';
 import showWeatherDetails from '../utils/showWeatherDetails';
+import DogGrid from './DogGrid';
 
 export default function Forecast({ hour }: { hour: HourProp }): JSX.Element {
   if (!hour) return <></>;
@@ -53,20 +54,7 @@ export default function Forecast({ hour }: { hour: HourProp }): JSX.Element {
         setTemperature={setTemperature}
       />
 
-      <div data-hour-id={hour.time_epoch} className="weather-images">
-        {images.map((image: string, i: number) => {
-          const alt: string = image.split('/')[3];
-          const imageClass: string = alt ? 'dog' : 'dog opaque';
-          return (
-            <img
-              key={`${image}-${i}`}
-              className={imageClass}
-              src={image}
-              alt={alt}
-            />
-          );
-        })}
-      </div>
+      <DogGrid hour={hour} images={images} />
 
       <span id="carrat" className="down"></span>
       <table id="weather-details-mobile"></table>
