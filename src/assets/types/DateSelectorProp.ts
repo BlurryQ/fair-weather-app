37c64e4da248c6a69af073fa7a424dba551ba20a
BlurryQ <jazz.barlow@outlook.com>
2025-02-06 11:27:49 +0000

    Removes defunct  variables

--- src/assets/types/DateSelectorProp.ts
+++ src/assets/types/DateSelectorProp.ts
@@ -1,9 +1,7 @@
 export type DateSelectorProp = {
     chosenDay: number,
     setChosenDay: React.Dispatch<React.SetStateAction<number>>,
+    setChosenHour: React.Dispatch<React.SetStateAction<number>>,
     dateEpoch: number,
     setDateEpoch: React.Dispatch<React.SetStateAction<number>>,
-    dateString: string,
-    setDateString: React.Dispatch<React.SetStateAction<string>>,
-    setChosenHour: React.Dispatch<React.SetStateAction<number>>,
   }
