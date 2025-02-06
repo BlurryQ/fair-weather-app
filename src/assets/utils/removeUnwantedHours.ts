eb79f43d1181231f4de40a694a3e5bc5a966440a
BlurryQ <jazz.barlow@outlook.com>
2025-02-06 11:26:14 +0000

    Alters function for one dimensional array

--- src/assets/utils/removeUnwantedHours.ts
+++ src/assets/utils/removeUnwantedHours.ts
@@ -1,23 +1,24 @@
-import { HourProp } from "../types/HourProp";
 import { format } from "date-fns"
 
-export default function removeUnwantedHours(hours: HourProp[]): HourProp[] {
+// props
+import { HourProp } from "../types/HourProp";
+
+export default function removeUnwantedHours(hours: HourProp): boolean {
+
+// console.log(hours)
     const earliestTime: number = 9
     const latestTime: number = 22
 
-    const [hoursDate]: string[] = hours[0].time.split(" ")
+    const [hoursDate]: string[] = hours.time.split(" ")
 
     const today: Date = new Date()
     const todayFormattedDate = format(today, "yyyy-MM-dd")
     const currentHour: number = today.getHours()
 
-    const relevantHours: HourProp[] = hours.filter((hour: HourProp) => {
-        const epochHour: number = new Date(hour.time_epoch * 1000).getHours()
-        const outsideRelevantHours: boolean = epochHour < earliestTime || epochHour > latestTime
-        const isToday: boolean = todayFormattedDate === hoursDate
+    const epochHour: number = new Date(hours.time_epoch * 1000).getHours()
+    const outsideRelevantHours: boolean = epochHour < earliestTime || epochHour > latestTime
+    const isToday: boolean = todayFormattedDate === hoursDate
 
-        if ((isToday && currentHour > epochHour) || outsideRelevantHours) return false
-        return true
-    })
-    return relevantHours
+    if ((isToday && currentHour > epochHour) || outsideRelevantHours) return false
+    return true
 }
