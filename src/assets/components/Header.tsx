0d69d9b399ce846544badf5bdf5126bc63181968
BlurryQ <jazz.barlow@outlook.com>
2025-02-06 11:31:48 +0000

    Adds component and section

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,5 +1,22 @@
 import '../styles/header.css';
 
-export default function Header(): JSX.Element {
-  return <h1 className="header">Fair Weather App</h1>;
+// components
+import Location from './Location';
+
+// types
+import { WeatherDataProp } from '../types/WeatherDataProp';
+
+export default function Header({
+  setWeatherData,
+  setLoading,
+}: {
+  setWeatherData: React.Dispatch<React.SetStateAction<WeatherDataProp | null>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}): JSX.Element {
+  return (
+    <header>
+      <h1 className="header">Fair Weather App</h1>
+      <Location setWeatherData={setWeatherData} setLoading={setLoading} />
+    </header>
+  );
 }
