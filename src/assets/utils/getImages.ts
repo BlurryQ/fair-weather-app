da007e6dbba1e5df8406bd7bc34285aea5efb756
BlurryQ <jazz.barlow@outlook.com>
2025-02-09 17:00:45 +0000

    Adds easy access variables

--- src/assets/utils/getImages.ts
+++ src/assets/utils/getImages.ts
@@ -10,25 +10,33 @@ import sunnyDobe from "/images/sunny.png"
 import windy from "/images/windy.png"
 import placeholder from "/favicon.png"
 
+
 // props
 import { HourProp } from "../types/HourProp";
 
 export default function getImages(weather: HourProp): string[] {
+    const rainPercentageTrigger: number = 60
+    const tooHotTrigger: number = 18
+    const tooColdTrigger: number = 5
+    const uvTrigger: number = 3
+    const highWindTrigger: number = 35
+    const windTrigger: number = 20
+    const visibilityTrigger: number = 2
     const images = []
     
     if (weather.condition.text.includes("snow")) images.push(snowyDobe)
-        else if (weather.will_it_rain || weather.chance_of_rain >= 60) images.push(rainyDobe)
+        else if (weather.will_it_rain || weather.chance_of_rain >= rainPercentageTrigger) images.push(rainyDobe)
     
-    if (weather.temp_c > 18) images.push(sunnyDobe)
-    else if (weather.temp_c < 5) images.push(coldDobe)
+    if (weather.temp_c > tooHotTrigger) images.push(sunnyDobe)
+    else if (weather.temp_c < tooColdTrigger) images.push(coldDobe)
     else images.push(jacketDobe)
 
-    if (weather.uv > 3) images.push(burntDobe)
+    if (weather.uv > uvTrigger) images.push(burntDobe)
 
-    if (weather.wind_mph >= 35) images.push(hurricane)
-    else if (weather.wind_mph >= 20) images.push(windy)
+    if (weather.wind_mph >= highWindTrigger) images.push(hurricane)
+    else if (weather.wind_mph >= windTrigger) images.push(windy)
 
-    if (weather.vis_miles <= 2) images.push(foggyDobe)
+    if (weather.vis_miles <= visibilityTrigger) images.push(foggyDobe)
 
     while (images.length < 4) images.push(placeholder)
 
