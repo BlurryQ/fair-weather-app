d3666cfa09e7c2571e21dfb1dfdd17dbbb8f7a65
BlurryQ <jazz.barlow@outlook.com>
2025-02-11 14:34:20 +0000

    Adds state to prop

--- src/assets/types/LocationListProp.ts
+++ src/assets/types/LocationListProp.ts
@@ -1,10 +1,11 @@
 import { Autocomplete } from './Autocomplete';
 
 export type LocationListProp = {
-    autocomplete: Autocomplete[];
-    setAutocomplete: React.Dispatch<React.SetStateAction<Autocomplete[] | null>>;
-    setLocation: React.Dispatch<React.SetStateAction<string>>;
-    setLongitude: React.Dispatch<React.SetStateAction<number>>;
-    setLatitude: React.Dispatch<React.SetStateAction<number>>;
-  };
+  autocomplete: Autocomplete[];
+  setAutocomplete: React.Dispatch<React.SetStateAction<Autocomplete[]>>;
+  setLocation: React.Dispatch<React.SetStateAction<string>>;
+  setLongitude: React.Dispatch<React.SetStateAction<number>>;
+  setLatitude: React.Dispatch<React.SetStateAction<number>>;
+  highlightedIndex: number,
+};
 
