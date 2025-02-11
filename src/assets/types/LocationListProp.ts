3238ed11244fea3fa8ea251ef051f5ba7e75b5df
BlurryQ <jazz.barlow@outlook.com>
2025-02-11 14:49:12 +0000

    Removes unecessary setters

--- src/assets/types/LocationListProp.ts
+++ src/assets/types/LocationListProp.ts
@@ -2,10 +2,7 @@ import { Autocomplete } from './Autocomplete';
 
 export type LocationListProp = {
   autocomplete: Autocomplete[];
-  setAutocomplete: React.Dispatch<React.SetStateAction<Autocomplete[]>>;
-  setLocation: React.Dispatch<React.SetStateAction<string>>;
-  setLongitude: React.Dispatch<React.SetStateAction<number>>;
-  setLatitude: React.Dispatch<React.SetStateAction<number>>;
   highlightedIndex: number,
+  displayLocationData: Function
 };
 
