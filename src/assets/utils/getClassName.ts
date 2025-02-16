3f7643dd21b70a671fa2f4a8b7814772614179aa
BlurryQ <jazz.barlow@outlook.com>
2025-02-16 12:49:26 +0000

    Corrects class adding for placeholder

--- src/assets/utils/getClassName.ts
+++ src/assets/utils/getClassName.ts
@@ -1,14 +1,13 @@
 export default function getClassName(index: number, isPlaceholder: boolean): string {
-    let className: string = 'forecast';
-    if (index === 0) className += ' far-left';
-    if (index === 4) className += ' far-right';
-    if (index === 0 || index === 1) className += ' left';
-    else if (index === 2) className += ' center';
-    else if (index === 3 || index === 4) className += ' right';
-    // if 0 index placeholder show blank page, else logo
-    if (isPlaceholder) {
-      index === 0 ? className += ' out-of-range-logo' : ' out-of-range'
-    }
-
-    return className;
-  }
+  let className: string = 'forecast';
+  if (index === 0) className += ' far-left';
+  if (index === 4) className += ' far-right';
+  if (index === 0 || index === 1) className += ' left';
+  else if (index === 2) className += ' center';
+  else if (index === 3 || index === 4) className += ' right';
+  // if placeholder show blank page and  logo
+  if (isPlaceholder)
+    className += ' out-of-range'
+  // console.log(className)
+  return className;
+}
