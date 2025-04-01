a527402eda97ef1abd2e0a5aff1e46d0e17c3379
BlurryQ <jazz.barlow@outlook.com>
2025-04-01 20:43:51 +0100

    Add NavBar component for user authentication and settings navigation

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+export default function NavBar() {
+  return (
+    <ul>
+      <li>
+        <a href="/settings" className="settings-link">
+          Sign Up
+        </a>
+      </li>
+      <li>
+        <a href="/settings" className="settings-link">
+          Log In
+        </a>
+      </li>
+      <li>
+        <a href="/settings" className="settings-link">
+          Settings
+        </a>
+      </li>
+    </ul>
+  );
+}
