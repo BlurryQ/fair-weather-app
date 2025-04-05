746d6b9c9c74074cf942028b10785249ddf7303a
BlurryQ <jazz.barlow@outlook.com>
2025-04-05 17:34:59 +0100

    Implement conditional rendering for user authentication links in NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,40 @@
+import '../styles/navBar.css';
+
 export default function NavBar() {
+  // get current user
+
+  // TODO - get user from context
+  const user: boolean = true;
+
   return (
-    <ul>
-      <li>
-        <a href="/settings" className="settings-link">
-          Sign Up
-        </a>
-      </li>
-      <li>
-        <a href="/settings" className="settings-link">
-          Log In
-        </a>
-      </li>
-      <li>
-        <a href="/settings" className="settings-link">
-          Settings
-        </a>
-      </li>
+    <ul className="nav-bar">
+      {user ? (
+        <>
+          <li>
+            <a href="/settings" className="settings-link">
+              Settings
+            </a>
+          </li>
+          <li>
+            <a href="/logout" className="settings-link">
+              Logout
+            </a>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <a href="/signup" className="settings-link">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a href="/login" className="settings-link">
+              Log In
+            </a>
+          </li>
+        </>
+      )}
     </ul>
   );
 }
