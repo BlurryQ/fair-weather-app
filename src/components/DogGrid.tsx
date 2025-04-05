b88f62153edda2210083af9bd140e8e4280d1596
BlurryQ <jazz.barlow@outlook.com>
2025-04-05 17:34:11 +0100

    Fix alt text extraction logic for dog images in DogGrid component

--- src/components/DogGrid.tsx
+++ src/components/DogGrid.tsx
@@ -12,7 +12,7 @@ export default function DogGrid({
     <div data-hour-id={hour.time_epoch} className="weather-images">
       {images.map((image: string, i: number) => {
         // split at [2] for placeholder due to different location (eg. /fair-weather-app/images/rainy.png)
-        let alt: string = image.split('/')[3] || image.split('/')[2];
+        let alt: string = image.split('/')[5] || image.split('/')[1];
         alt = alt.split('.')[0];
         const imageClass: string = alt === 'favicon' ? 'dog opaque' : 'dog';
         return (
