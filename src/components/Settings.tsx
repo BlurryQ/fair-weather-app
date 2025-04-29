4a0a342f3dfd764fb9d1dc6fd77b0613aabd0168
BlurryQ <jazz.barlow@outlook.com>
2025-04-29 20:51:01 +0100

    Implement Settings component with grid layout and multiple SettingsCard instances

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,3 +1,14 @@
+import '../styles/settings.css';
+import SettingsCard from './SettingsCard';
+
 export default function Settings() {
-  return <>hi</>;
+  // TODO work out where (component?) to get userdata from local storage
+
+  return (
+    <div className="settings">
+      <SettingsCard />
+      <SettingsCard />
+      <SettingsCard />
+    </div>
+  );
 }
