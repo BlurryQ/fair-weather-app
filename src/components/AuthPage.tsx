20428966eb0290053476b70ae93085a64af241dc
BlurryQ <jazz.barlow@outlook.com>
2025-05-02 20:16:58 +0100

    Add AuthPage component for unified authentication interface

---
+++ src/components/AuthPage.tsx
@@ -0,0 +1,56 @@
+import { useLocation } from 'react-router-dom';
+import '../styles/auth.css';
+import { useState } from 'react';
+
+export default function AuthPage() {
+  const location = useLocation();
+  const heading = location.pathname === '/signup' ? 'Sign Up' : 'Log In';
+  const buttonText = location.pathname === '/signup' ? 'Sign Up' : 'Log In';
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { placeholder, value } = e.target;
+    if (placeholder === 'Email') {
+      setEmail(value);
+    } else if (placeholder === 'Password') {
+      console.log(value);
+      setPassword(value);
+    } else if (placeholder === 'Confirm Password') {
+      console.log(value);
+      setConfirmPassword(value);
+    }
+  };
+
+  return (
+    <div className="auth">
+      <h1>{heading}</h1>
+      <form className="auth">
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={password}
+        />
+        {location.pathname === '/signup' && (
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            value={confirmPassword}
+          />
+        )}
+        <button type="submit">{buttonText}</button>
+      </form>
+    </div>
+  );
+}
