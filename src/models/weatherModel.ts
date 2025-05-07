897054cba602347afb28ff23263338462bef234f
BlurryQ <jazz.barlow@outlook.com>
2025-05-07 19:35:54 +0100

    Fix API key reference in weather model to use VITE_WEATHER_API

--- src/models/weatherModel.ts
+++ src/models/weatherModel.ts
@@ -3,13 +3,13 @@ import axios, { AxiosResponse } from "axios";
 export function getAutocompleteWeather(query: string): Promise<AxiosResponse>
 {
 
-    const api_key: string = import.meta.env.VITE_API_KEY;
+    const api_key: string = import.meta.env.VITE_WEATHER_API;
     const url: string = `https://api.weatherapi.com/v1/search.json?key=${api_key}&q=${query}`;
     return axios.get(url)
 }
 
 export function getLatandLongWeather(latitude: number, longitude: number): Promise<AxiosResponse> {
-    const api_key: string = import.meta.env.VITE_API_KEY;
+    const api_key: string = import.meta.env.VITE_WEATHER_API;
     const url: string = `https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${latitude},${longitude}&days=3`;
     return axios.get(url)
 }
