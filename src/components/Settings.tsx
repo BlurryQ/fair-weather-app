8b1bd83d2a18b3b09a6d1b8952ebad816d5e25a2
BlurryQ <jazz.barlow@outlook.com>
2025-05-07 19:36:34 +0100

    Display user email in Settings component after retrieving from UserContext

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,13 +1,18 @@
+import { useUser } from '../context/UserContext';
 import '../styles/settings.css';
 import SettingsCard from './SettingsCard';
 
 export default function Settings() {
   // TODO work out where (component?) to get userdata from local storage
 
+  const userContext = useUser();
+  if (!userContext) return;
+  const { user } = userContext;
+
   return (
     <>
       {/* TODO create drop down component which accepts value and options */}
-
+      <h1>{user.email}</h1>
       <div className="settings">
         <SettingsCard />
         <SettingsCard />
