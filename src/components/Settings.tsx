f3f5ce108794b58af5f639fc7a96de94d30d6e2d
BlurryQ <jazz.barlow@outlook.com>
2025-05-12 21:07:20 +0100

    Refactor Settings component to integrate user context and enhance settings options

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import { useUser } from '../context/UserContext';
 import '../styles/settings.css';
 import SettingsCard from './SettingsCard';
 
 export default function Settings() {
   // TODO work out where (component?) to get userdata from local storage
+  const [firstHour, setFirstHour] = useState<number>(9);
+  const [lastHour, setLastHour] = useState<number>(22);
+  const [isCelsius, setIsCelsius] = useState<boolean>(true);
+  const [isMiles, setIsMiles] = useState<boolean>(true);
 
   const userContext = useUser();
   if (!userContext) return;
@@ -12,7 +17,54 @@ export default function Settings() {
   return (
     <>
       {/* TODO create drop down component which accepts value and options */}
-      <h1>{user.email}</h1>
+      <div className="core-settings">
+        <h1>{user.email}</h1>
+
+        <label htmlFor="first-hour">First Hour:</label>
+        <input
+          id="first-hour"
+          type="number"
+          min={0}
+          max={23}
+          defaultValue={firstHour}
+          onChange={(e) => setFirstHour(parseInt(e.target.value))}
+        />
+
+        <label htmlFor="last-hour">Last Hour:</label>
+        <input
+          id="last-hour"
+          type="number"
+          min={0}
+          max={23}
+          defaultValue={lastHour}
+          onChange={(e) => setLastHour(parseInt(e.target.value))}
+        />
+
+        <label htmlFor="is-celsius">
+          Temperature:
+          <input
+            className="switch"
+            id="is-celsius"
+            type="checkbox"
+            onChange={() => setIsCelsius(!isCelsius)}
+            checked={isCelsius}
+          />
+          <span className="slider"></span>
+        </label>
+
+        <label htmlFor="is-miles">
+          Distance:
+          <input
+            className="switch"
+            id="is-miles"
+            type="checkbox"
+            onChange={() => setIsMiles(!isMiles)}
+            checked={isMiles}
+          />
+          <span className="slider"></span>
+        </label>
+      </div>
+
       <div className="settings">
         <SettingsCard />
         <SettingsCard />
