93385cae18cc7c128a3faa958fa7297ea9c6c25e
BlurryQ <jazz.barlow@outlook.com>
2025-05-15 21:02:04 +0100

    Refactor SettingsCard component to add index prop and manage card color state

--- src/components/SettingsCard.tsx
+++ src/components/SettingsCard.tsx
@@ -1,14 +1,31 @@
 import '../styles/settingsCard.css';
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 
 // remove image default?
 import defaultImage from '../assets/images/weather/sunny.png';
 import Toggle from './Toggle';
 
-export default function SettingsCard() {
+export default function SettingsCard({ index }: { index: number }) {
   const [value, setValue] = useState<number>(9);
   const [image, setImage] = useState<string>(defaultImage);
   const [isSettingOn, setIsSettingOn] = useState<boolean>(true);
+
+  const changeCardColor = (isSettingOn: boolean) => {
+    const card = document.querySelectorAll('.settings-card');
+    console.dir(card[index]);
+    if (card) {
+      if (card[index].classList[1] === 'disabled') {
+        console.log('removed');
+        card[index].classList.remove('disabled');
+      } else {
+        card[index].classList.add('disabled');
+      }
+    }
+  };
+
+  useEffect(() => {
+    changeCardColor(isSettingOn);
+  }, [isSettingOn]);
 
   const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -24,7 +41,11 @@ export default function SettingsCard() {
 
   return (
     <div className="settings-card">
-      <Toggle state={isSettingOn} setState={setIsSettingOn} label="card" />
+      <Toggle
+        state={isSettingOn}
+        setState={setIsSettingOn}
+        label={'card-' + index}
+      />
 
       <img src={image} alt="Uploaded" />
 
