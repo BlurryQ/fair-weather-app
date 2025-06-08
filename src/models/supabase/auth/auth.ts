d74ef6676a2af41b0c1080fc340dd40b2d316cb0
BlurryQ <jazz.barlow@outlook.com>
2025-06-08 16:41:35 +0100

    Add user authentication functions for sign up, sign in, and sign out

---
+++ src/models/supabase/auth/auth.ts
@@ -0,0 +1,48 @@
+import supabase from "../client";
+
+  export async function signUpUser(email: string, password: string) {
+    try {
+      let { data, error } = await supabase.auth.signUp({ email, password });
+      if (error) throw error;
+      console.log('user', data.user);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }
+
+  export async function signInUser(email: string, password: string, login: any) {
+    try {
+      let { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) throw error;
+      console.log('>> user', data.user);
+      login(data.user)
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }
+
+  export async function signOutUser() {
+    try {
+      let { error } = await supabase.auth.signOut();
+      if (error) throw error;
+      console.log('User signed out');
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }
+
+  // export async function resetPassword(email: string) {
+  //   try {
+  //     const { data, error } = await supabase.auth.api.resetPasswordForEmail(
+  //       email,
+  //       { redirectTo: 'https://example.com/update-password' }
+  //     )
+  //     if (error) throw error;
+  //     console.log('Password reset email sent', data);
+  //   } catch (err: any) {
+  //     console.error(err.message);
+  //   }
+  // }  
