6648b808db5efb0c4eca7963cf2f29fd073773a4
BlurryQ <jazz.barlow@outlook.com>
2025-06-09 20:57:57 +0100

    Refactor SettingsCard component to accept setting prop and update card title dynamically

--- src/components/SettingsCard.tsx
+++ src/components/SettingsCard.tsx
@@ -5,24 +5,31 @@ import { useState, ChangeEvent, useEffect } from 'react';
 import defaultImage from '../assets/images/weather/sunny.png';
 import Toggle from './Toggle';
 
-export default function SettingsCard({ index }: { index: number }) {
-  const [value, setValue] = useState<number>(9);
+// types
+import { SettingdCardData } from '../types/settings/SettingsCardData';
+
+export default function SettingsCard({
+  index,
+  setting,
+}: {
+  index: number;
+  setting: SettingdCardData;
+}) {
+  const [value, setValue] = useState<number>(setting.value); // Example value, can be changed
   const [image, setImage] = useState<string>(defaultImage);
   const [isSettingOn, setIsSettingOn] = useState<boolean>(true);
 
-  const changeCardColor = (isSettingOn: boolean) => {
+  const changeCardColor = () => {
     const card = document.querySelectorAll('.settings-card');
-    if (card) {
-      if (card[index].classList[1] === 'disabled') {
-        card[index].classList.remove('disabled');
-      } else {
-        card[index].classList.add('disabled');
-      }
+    if (card[index].classList[1] === 'disabled') {
+      card[index].classList.remove('disabled');
+    } else {
+      card[index].classList.add('disabled');
     }
   };
 
   useEffect(() => {
-    changeCardColor(isSettingOn);
+    changeCardColor();
   }, [isSettingOn]);
 
   const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
@@ -47,7 +54,7 @@ export default function SettingsCard({ index }: { index: number }) {
 
       <img src={image} alt="Uploaded" />
 
-      <div>TITLE</div>
+      <div>{setting.name}</div>
 
       <input type="number" value={value} onChange={handleValueChange} />
 
@@ -57,6 +64,7 @@ export default function SettingsCard({ index }: { index: number }) {
         onChange={handleImageUpload}
         className="file-input"
       />
+
       <button className="save">Save</button>
     </div>
   );
