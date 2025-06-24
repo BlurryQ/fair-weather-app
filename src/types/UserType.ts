bb9c8c903f6881298709e02049acb7781f0ffb7b
BlurryQ <jazz.barlow@outlook.com>
2025-06-24 20:04:26 +0100

    Add optional settings property to UserType type definition

--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,4 +1,11 @@
+import { CoreSettings } from './settings/CoreSettings';
+import { ImageSettings } from './settings/ImageSettings';
+
 export type UserType = {
     id: string,
     email: string,
+    settings?: {
+        coreSettings? :CoreSettings,
+        imageSetteings? :ImageSettings
+    }
 }
