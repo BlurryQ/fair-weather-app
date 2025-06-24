6f6b4f815d3d08fe9bbfaba6b80ce0c0a82cebe9
BlurryQ <jazz.barlow@outlook.com>
2025-06-24 20:04:40 +0100

    Add user settings fetching and update functionality in UserContext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,11 +1,37 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from 'react';
+
+// models
+import getAllSettings from '../models/supabase/tables/getAllSettings';
+import { updateCoreSettings } from '../models/supabase/tables/coreSettings';
+
+// types
+import { AllSettings } from '../types/settings/AllSettings';
+import { CoreSettings } from '../types/settings/CoreSettings';
+import { ImageSettings } from '../types/settings/ImageSettings';
 import { UserType } from '../types/UserType';
-const noUser: UserType = { id: '', email: '' };
+
+// utils
+import validateSettings from '../utils/validateSettings';
+
+const noUser: UserType = {
+  id: '',
+  email: '',
+};
 
 type UserContextType = {
   user: UserType;
   login: (userData: UserType) => void;
   logout: () => void;
+  updateUserSettings: (
+    settingType: string,
+    userUpdates: AllSettings | CoreSettings | ImageSettings
+  ) => void;
 };
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
@@ -15,11 +41,25 @@ type UserProviderProps = {
 };
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
-  // Load user data from localStorage on initial render
   const [user, setUser] = useState<UserType>(() => {
     const savedUser = localStorage.getItem('user');
     return savedUser ? JSON.parse(savedUser) : noUser;
   });
+
+  useEffect(() => {
+    const fetchSettings = async () => {
+      try {
+        const settings = await getAllSettings();
+        updateUserSettings('all', settings);
+      } catch (error) {
+        console.error('Failed to fetch settings:', error);
+      }
+    };
+
+    if (user.id && !validateSettings(user.settings)) {
+      fetchSettings();
+    }
+  }, [user]);
 
   const login = (userData: UserType) => {
     setUser(userData);
@@ -31,8 +71,33 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     localStorage.removeItem('user');
   };
 
+  const updateUserSettings = (
+    settingsType: string,
+    settings: AllSettings | CoreSettings | ImageSettings
+  ): void => {
+    setUser((prevUser: any) => {
+      let updatedUser = { ...prevUser, settings: { ...settings } };
+      if (settingsType === 'images')
+        updatedUser.settings.imageSettings = settings as ImageSettings;
+      else if (settingsType === 'all')
+        updatedUser.settings = settings as AllSettings;
+      else if (settingsType === 'core') {
+        updateCoreSettings(settings as CoreSettings).then((response) => {
+          if (response) {
+            updatedUser.settings.coreSettings = settings as CoreSettings;
+          } else {
+            console.error('Failed to update core settings');
+          }
+        });
+      }
+
+      localStorage.setItem('user', JSON.stringify(updatedUser));
+      return updatedUser;
+    });
+  };
+
   return (
-    <UserContext.Provider value={{ user, login, logout }}>
+    <UserContext.Provider value={{ user, login, logout, updateUserSettings }}>
       {children}
     </UserContext.Provider>
   );
