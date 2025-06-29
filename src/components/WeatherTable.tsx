abec80a242e2f2e5e9e5ffa9aa1a425f1a2de5ab
BlurryQ <jazz.barlow@outlook.com>
2025-06-29 20:25:07 +0100

    Refactor WeatherTable component to streamline weather data handling and improve readability

--- src/components/WeatherTable.tsx
+++ src/components/WeatherTable.tsx
@@ -1,72 +1,58 @@
 import '../styles/forecast.css';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // types
 import { HourProp } from '../types/HourProp';
+import { useUser } from '../context/UserContext';
 
 export default function WeatherTable({
   hour,
-  temperature,
-  setTemperature,
 }: {
   hour: HourProp;
-  temperature: number;
-  setTemperature: React.Dispatch<React.SetStateAction<number>>;
 }): JSX.Element | null {
-  if (!hour) return null;
-  const [condition, setCondition] = useState<string>('');
-  const [rain, setRain] = useState<number>(0);
-  const [rainChance, setRainChance] = useState<number>(0);
-  const [snow, setSnow] = useState<number>(0);
-  const [snowChance, setSnowChance] = useState<number>(0);
-  const [feelsLike, setFeelsLike] = useState<number>(0);
-  const [windSpeed, setWindSpeed] = useState<number>(0);
-  const [uvIndex, setUvIndex] = useState<number>(0);
-  const [visability, setVisability] = useState<number>(0);
+  const [weatherData, setWeatherData] = useState<{}>({});
+  const userContext = useUser();
+  if (!userContext || !hour) return null;
+  const { user } = userContext;
+  let useCelcius: boolean = true;
+  let useMiles: boolean = true;
+  if (user.hasOwnProperty('settings')) {
+    useCelcius = user.settings?.coreSettings?.is_celsius ?? true;
+    useMiles = user.settings?.coreSettings?.is_miles ?? true;
+  }
 
-  const setWeatherData = (hour: HourProp) => {
+  const getWeatherDataMetrics = (hour: HourProp) => {
     const {
-      chance_of_rain,
-      chance_of_snow,
-      condition,
       feelslike_c,
       feelslike_f,
       wind_mph,
       wind_kph,
       temp_c,
       temp_f,
-      time,
-      time_epoch,
-      uv,
       vis_miles,
       vis_km,
-      will_it_rain,
-      will_it_snow,
     } = hour;
 
-    // TODO: if user chooses celcius/fahrenheit set appropriate values
-    const temp: number = temp_c;
-    const feelsLikeTemp: number = feelslike_c;
+    const temp: string = useCelcius
+      ? `${temp_c}°C (${feelslike_c}°C)`
+      : `${temp_f}°F (${feelslike_f}°F)`;
 
-    // TODO: if user chooses miles/kilometer set appropriate values
-    const wind: number = wind_mph;
-    const visability: number = vis_miles;
+    const wind: string = useMiles ? wind_mph + 'mph' : wind_kph + 'kmh';
+    const visability: string = useMiles
+      ? vis_miles + ' miles'
+      : vis_km + ' kilometers';
 
-    setCondition(condition.text);
-    setRain(will_it_rain);
-    setRainChance(chance_of_rain);
-    setSnow(will_it_snow);
-    setSnowChance(chance_of_snow);
-    setTemperature(temp);
-    setFeelsLike(feelsLikeTemp);
-    setWindSpeed(wind);
-    setUvIndex(uv);
-    setVisability(visability);
+    setWeatherData({
+      ...hour,
+      temperature: temp,
+      windSpeed: wind,
+      visability: visability,
+    });
   };
 
   useEffect(() => {
     if (!hour) return;
-    setWeatherData(hour);
+    getWeatherDataMetrics(hour);
   }, [hour]);
 
   return (
@@ -74,7 +60,7 @@ export default function WeatherTable({
       <thead>
         <tr>
           <th colSpan={2} className="table-header">
-            {condition}
+            {weatherData.condition.text}
           </th>
         </tr>
       </thead>
@@ -83,33 +69,33 @@ export default function WeatherTable({
           <th>Rain:</th>
           <td>
             {' '}
-            {rain ? 'Yes' : 'No'} ({rainChance}%)
+            {weatherData.will_it_rain ? 'Yes' : 'No'} (
+            {weatherData.chance_of_rain}%)
           </td>
         </tr>
         <tr>
           <th>Snow:</th>
           <td>
             {' '}
-            {snow ? 'Yes' : 'No'} ({snowChance}%)
+            {weatherData.will_it_snow ? 'Yes' : 'No'} (
+            {weatherData.chance_of_snow}%)
           </td>
         </tr>
         <tr>
           <th>Temp:</th>
-          <td>
-            {temperature}°C ({feelsLike}°C)
-          </td>
+          <td>{weatherData.temperature}</td>
         </tr>
         <tr>
           <th>Wind:</th>
-          <td>{windSpeed} mph</td>
+          <td>{weatherData.windSpeed}</td>
         </tr>
         <tr>
           <th>View:</th>
-          <td>{visability} miles</td>
+          <td>{weatherData.visability}</td>
         </tr>
         <tr>
           <th>UV:</th>
-          <td>{uvIndex}</td>
+          <td>{weatherData.uv}</td>
         </tr>
       </tbody>
     </table>
