04faf2d4d02eab8d7708fbd35916b9c2585d56c1
BlurryQ <jazz.barlow@outlook.com>
2025-07-05 19:30:52 +0100

    completed

--- src/models/supabase/tables/getAllSettings.ts
+++ src/models/supabase/tables/getAllSettings.ts
@@ -7,7 +7,6 @@ import { AllSettings } from '../../../types/settings/AllSettings';
 import { CoreSettings } from '../../../types/settings/CoreSettings';
 import { ImageSettings } from '../../../types/settings/ImageSettings';
 
-// TODO fix type
 export default async function getAllSettings(): Promise<AllSettings> {
   const coreSettings: CoreSettings = await getCoreSettings();
   const imageSettings: ImageSettings = await getImageSettings();
