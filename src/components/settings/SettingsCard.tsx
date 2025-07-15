4427666703804f7d617b82f63687df2ce01865e4
BlurryQ <jazz.barlow@outlook.com>
2025-07-15 19:39:27 +0100

    Moved and updated function with back end interactions via components

---
+++ src/components/settings/SettingsCard.tsx
@@ -0,0 +1,103 @@
+import '../../styles/settingsCard.css';
+import { useState, ChangeEvent, useEffect } from 'react';
+
+// remove image default?
+import defaultImage from '../../assets/images/weather/sunny.png';
+import Toggle from '../Toggle';
+
+// types
+import { SettingdCardData } from '../../types/settings/SettingsCardData';
+import capitalisedEachWord from '../../utils/capitalisedEachWord';
+import SaveButton from '../general/SaveButton';
+import { ImageSettings } from '../../types/settings/ImageSettings';
+import { formatImageSettingsForDB } from '../../utils/formatImageSettings';
+
+export default function SettingsCard({
+  index,
+  setting,
+  imageSettings,
+}: {
+  index: number;
+  setting: SettingdCardData;
+  imageSettings: ImageSettings;
+}) {
+  const [value, setValue] = useState<number>(setting.value);
+  // TODO storage api request for image // save on context?
+  const [image, setImage] = useState<string>(defaultImage);
+
+  // TODO if setting === active, set isSettingOn to true
+  // console.log(setting.active);
+  const [isSettingOn, setIsSettingOn] = useState<boolean>(setting.active);
+
+  // build settings object
+  // dynamically update with values
+  // pass this to SaveButton
+  const [settings, setSettings] = useState<SettingdCardData>({
+    name: setting.name,
+    active: setting.active,
+    value: setting.value,
+  });
+
+  const changeCardColor = () => {
+    const cards = document.querySelectorAll('.settings-card');
+    // TODO get card to toggle correct color on first toggle when false
+    if (cards[index].classList[1] === 'disabled') {
+      cards[index].classList.remove('disabled');
+    } else {
+      cards[index].classList.add('disabled');
+    }
+  };
+
+  useEffect(() => {
+    changeCardColor();
+  }, [isSettingOn]);
+
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+
+  const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // TODO remove value in favour of setting?
+    setValue(Number(e.target.value));
+    setting['value'] = Number(e.target.value);
+    imageSettings = formatImageSettingsForDB(
+      setting,
+      imageSettings
+    ) as ImageSettings;
+  };
+
+  return (
+    <div className={'settings-card'}>
+      <Toggle
+        state={isSettingOn}
+        setState={setIsSettingOn}
+        label={'card-' + index}
+      />
+
+      <img src={image} alt="Uploaded" />
+
+      <div>{capitalisedEachWord(setting.name)}</div>
+
+      <input
+        id={setting.name + '_value'}
+        type="number"
+        value={value}
+        onChange={handleValueChange}
+        className={setting.name === 'good_day' ? 'invisible' : ''}
+      />
+
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        className="file-input"
+      />
+
+      <SaveButton type="image" settings={imageSettings} />
+    </div>
+  );
+}
