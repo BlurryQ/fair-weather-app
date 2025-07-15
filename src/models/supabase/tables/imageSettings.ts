35f1b7a1ef9057ba51a3c222a59c9752506ed54d
BlurryQ <jazz.barlow@outlook.com>
2025-07-15 19:33:56 +0100

    Add updateImageSettings function to modify user image settings

--- src/models/supabase/tables/imageSettings.ts
+++ src/models/supabase/tables/imageSettings.ts
@@ -20,3 +20,28 @@ import supabase from "../client";
       console.error(err.message);
     }
   }
+    
+  export async function updateImageSettings(settings: any) {
+    console.log("from inside supabase call", settings);
+    try {
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+      
+      if (userError || !user) throw userError;
+
+      const { data, error } = await supabase
+      .from('image_settings')
+      .update(settings)
+      .eq('id', user.id)
+      .select();
+
+      if (error) throw error;
+      return data;
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }
+
+  
