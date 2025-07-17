4e4fd3dcf6a7115a823149184ded5db518d5059f
BlurryQ <jazz.barlow@outlook.com>
2025-07-17 17:19:58 +0100

    Renames images and enables metrics based on user settings

--- src/utils/getImages.ts
+++ src/utils/getImages.ts
@@ -1,22 +1,24 @@
 // images
-import lowUV from "../assets/images/weather/lowUV.png"
-import cold from "../assets/images/weather/cold.png"
-import lowVisability from "../assets/images/weather/lowVisability.png"
-import highWind from "../assets/images/weather/highWind.png"
-import mild from "../assets/images/weather/mild.png"
-import rainy from "../assets/images/weather/rainy.png"
-import snowy from "../assets/images/weather/snowy.png"
-import sunny from "../assets/images/weather/sunny.png"
-import windy from "../assets/images/weather/windy.png"
+import lowUV from "../assets/images/weather/high_uv.png"
+import cold from "../assets/images/weather/low_temp.png"
+import lowVisability from "../assets/images/weather/low_visability.png"
+import highWind from "../assets/images/weather/high_wind.png"
+import mild from "../assets/images/weather/high_temp.png"
+import rainy from "../assets/images/weather/rain_chance.png"
+import snowy from "../assets/images/weather/snow_chance.png"
+import sunny from "../assets/images/weather/good_day.png"
+import windy from "../assets/images/weather/low_wind.png"
 import placeholder from "/favicon.png"
-
 
 // props
 import { HourProp } from "../types/HourProp";
+import { ImageSettings } from "../types/settings/ImageSettings"
+import { CoreSettings } from "../types/settings/CoreSettings"
 
 export default function getImages(weather: HourProp): string[] {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
-    const imageSettings = user.settings?.imageSettings || {};
+    const settings = user.settings || {};
+    const imageSettings: ImageSettings = settings.image_settings || {};
     
     const rainPercentageTrigger: number = imageSettings.rain_chance || 60
     const snowPercentageTrigger: number = imageSettings.snow_chance || 60
@@ -26,11 +28,12 @@ export default function getImages(weather: HourProp): string[] {
     const lowWindTrigger: number = imageSettings.low_wind || 20
     const highWindTrigger: number = imageSettings.high_wind|| 35
     const visibilityTrigger: number = imageSettings.low_visability || 2
-
+    
     // TODO make these settings dynamic
-    const weatherTemp: number = weather.temp_c
-    const weatherWind: number = weather.wind_mph
-    const weatherVis: number = weather.vis_miles
+    const coreSettings: CoreSettings = settings.core_settings || {};
+    const weatherTemp: number = coreSettings.is_celsius ? weather.temp_c : weather.temp_f
+    const weatherWind: number = coreSettings.is_miles? weather.wind_mph : weather.wind_kph
+    const weatherVis: number = coreSettings.is_miles? weather.vis_miles : weather.vis_km
     const images = []
     
     if (weather.will_it_snow || weather.chance_of_snow >= snowPercentageTrigger) images.push(snowy)
