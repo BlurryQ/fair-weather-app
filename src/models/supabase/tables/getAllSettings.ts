92e6b872dee449769af58522d2a8994f6d602bd2
BlurryQ <jazz.barlow@outlook.com>
2025-07-25 20:57:08 +0100

    Refactor getAllSettings to include image URLs and timestamp in the returned settings

--- src/models/supabase/tables/getAllSettings.ts
+++ src/models/supabase/tables/getAllSettings.ts
@@ -6,9 +6,18 @@ import { getImageSettings } from './imageSettings';
 import { AllSettings } from '../../../types/settings/AllSettings';
 import { CoreSettings } from '../../../types/settings/CoreSettings';
 import { ImageSettings } from '../../../types/settings/ImageSettings';
+import { getAllImageUrls } from '../storage/imageStorage';
+import { ImageUrls } from '../../../types/settings/ImageUrls';
 
 export default async function getAllSettings(): Promise<AllSettings> {
   const coreSettings: CoreSettings = await getCoreSettings();
   const imageSettings: ImageSettings = await getImageSettings();
-  return ({ coreSettings, imageSettings });
+  const imageUrls: ImageUrls[] | undefined = await getAllImageUrls(imageSettings.id);
+  const allSettings: AllSettings = {
+    coreSettings,
+    imageSettings,
+    imageUrls: imageUrls || [],
+    timestamp: new Date().getTime(), // Set current timestamp
+  };
+  return (allSettings);
 };
