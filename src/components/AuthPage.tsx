746800531dbff7da707e91f517e3f351abff9a2e
BlurryQ <jazz.barlow@outlook.com>
2025-08-25 21:10:40 +0100

    Implements new auth pages logic

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -1,19 +1,27 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles/auth.css';
 import { useState } from 'react';
-import { signUpUser, signInUser } from '../models/supabase/auth/auth';
+import {
+  signUpUser,
+  signInUser,
+  resetPassword,
+  updatePassword,
+} from '../models/supabase/auth/auth';
 import { useUser } from '../context/UserContext';
+import capitalisedEachWord from '../utils/capitalisedEachWord';
 
 export default function AuthPage() {
   const location = useLocation();
   const navigate = useNavigate();
-  const heading = location.pathname === '/signup' ? 'Sign Up' : 'Log In';
-  const buttonText = location.pathname === '/signup' ? 'Sign Up' : 'Log In';
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
+  const pageName: string = capitalisedEachWord(
+    location.pathname.split('/').pop() as string
+  );
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [success, setSuccess] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
   const userContext = useUser();
   if (!userContext) return <></>;
   const { login } = userContext;
@@ -30,53 +38,83 @@ export default function AuthPage() {
   };
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    // TODO implement password checking logic
+    // TODO util function?
     e.preventDefault();
     setLoading(true);
-    if (location.pathname === '/login') {
+    setError('');
+    setSuccess('');
+    if (pageName === 'Log In') {
       const isSuccessful: boolean = await signInUser(email, password, login);
       if (!isSuccessful) {
         setError('Login failed. Please check your credentials.');
-        setLoading(false);
-        return;
       }
-      setLoading(true);
       navigate('/');
-    } else if (location.pathname === '/signup') {
+    } else if (pageName === 'Sign Up') {
       const isSuccessful: boolean = await signUpUser(email, password);
       if (!isSuccessful) {
         setError('Sign up failed. Please check your credentials.');
-        setLoading(false);
-        return;
-      }
-      setLoading(true);
-      navigate('/login');
-    }
+      } else
+        setSuccess(
+          'Sign up successful. Please check your email for a confirmation link.'
+        );
+    } else if (pageName === 'Reset Email') {
+      const isSuccessful: boolean = await resetPassword(email);
+      if (!isSuccessful)
+        setError(
+          'Cannot find this email address. Please check your credentials.'
+        );
+      else
+        setSuccess(
+          'If this email exists on our database an email will be sent.'
+        );
 
-    navigate('/');
+      setTimeout(() => {
+        setSuccess('');
+        setError('');
+      }, 1500);
+    } else if (pageName === 'Reset Password') {
+      const isSuccessful: any = await updatePassword(password);
+      if (!isSuccessful)
+        setError('Error updating password. Please try again later.');
+      else
+        setSuccess('Password updated successfully. Please log in to continue.');
+      navigate('/log_in');
+    }
+    return setLoading(false);
   };
 
+  const handlePasswordReset = async () => {
+    navigate('/reset_email');
+  };
+
+  // TODO pimp loading
   if (loading) {
     return <div className="loading">Loading...</div>;
   }
 
   return (
     <div className="auth">
-      <h1>{heading}</h1>
-      {error && <div className="error">{error}</div>}
+      <h1>{pageName === 'Reset Email' ? 'Reset Password' : pageName}</h1>
+      {pageName === 'Reset Email' && <p>Please enter your email address.</p>}
       <form className="auth">
-        <input
-          type="email"
-          placeholder="Email"
-          onChange={handleChange}
-          value={email}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={handleChange}
-          value={password}
-        />
-        {location.pathname === '/signup' && (
+        {pageName !== 'Reset Password' && (
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={email}
+          />
+        )}
+        {(pageName === 'Sign Up' || pageName !== 'Reset Email') && (
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={password}
+          />
+        )}
+        {(pageName === 'Sign Up' || pageName === 'Reset Password') && (
           <input
             type="password"
             placeholder="Confirm Password"
@@ -84,9 +122,20 @@ export default function AuthPage() {
             value={confirmPassword}
           />
         )}
+        {pageName === 'Log In' && (
+          <button
+            type="button"
+            onClick={handlePasswordReset}
+            className={'reset-password'}
+          >
+            Reset Password
+          </button>
+        )}
         <button type="submit" onClick={handleSubmit}>
-          {buttonText}
+          {pageName === 'Reset Email' ? 'Reset Password' : pageName}
         </button>
+        {error && <div className="error">{error}</div>}
+        {success && <div className="success">{success}</div>}
       </form>
     </div>
   );
