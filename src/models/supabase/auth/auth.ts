b7148aea6ad72c80e2e011bc798a17e4e4fe9253
BlurryQ <jazz.barlow@outlook.com>
2025-08-25 21:09:31 +0100

    Implements reset and update password functions

--- src/models/supabase/auth/auth.ts
+++ src/models/supabase/auth/auth.ts
@@ -2,9 +2,8 @@ import supabase from "../client";
 
   export async function signUpUser(email: string, password: string): Promise<boolean> {
     try {
-      let { data, error } = await supabase.auth.signUp({ email, password });
+      let { error } = await supabase.auth.signUp({ email, password });
       if (error) throw error;
-      console.log('user', data.user);
       return true
     } catch (err: any) {
       console.error(err.message);
@@ -19,7 +18,6 @@ import supabase from "../client";
         password,
       });
       if (error) throw error;
-      console.log('>> user', data.user);
       login(data.user)
       return true
     } catch (err: any) {
@@ -38,15 +36,30 @@ import supabase from "../client";
     }
   }
 
-  // export async function resetPassword(email: string) {
-  //   try {
-  //     const { data, error } = await supabase.auth.api.resetPasswordForEmail(
-  //       email,
-  //       { redirectTo: 'https://example.com/update-password' }
-  //     )
-  //     if (error) throw error;
-  //     console.log('Password reset email sent', data);
-  //   } catch (err: any) {
-  //     console.error(err.message);
-  //   }
-  // }  
+  export async function resetPassword(email: string) {
+    try {
+      const { error } = await supabase.auth.resetPasswordForEmail(
+        email,
+        { redirectTo: 'http://localhost:5173/reset_password' }
+      )
+      if (error) throw error;
+      return true
+    } catch (err: any) {
+      console.error(err.message);
+      return false
+    }
+  }  
+
+  export async function updatePassword(password: string) {
+    try {
+      const { error } = await supabase.auth.updateUser({
+        password
+      });
+      if (error) throw error;
+      console.log('User updated');
+      return true
+    } catch (err: any) {
+      console.error(err.message);
+      return false
+    }
+  }
