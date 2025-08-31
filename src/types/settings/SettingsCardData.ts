d127ed377ca84c5a69348fa6a6978d4cf1e1ba68
BlurryQ <jazz.barlow@outlook.com>
2025-08-31 21:29:26 +0100

    Adds types to type

--- src/types/settings/SettingsCardData.ts
+++ src/types/settings/SettingsCardData.ts
@@ -1,5 +1,15 @@
+import { ImageSettings } from "./ImageSettings"
+
+export type NumericSettingKeys = {
+  [key in keyof ImageSettings]: ImageSettings[key] extends number ? key : never
+}[keyof ImageSettings];
+
+export type BooleanSettingKeys = {
+    [key in keyof ImageSettings]: ImageSettings[key] extends boolean ? key : never
+}[keyof ImageSettings];
+
 export type SettingdCardData = {
-    name: string
-    active: boolean,   
-    value: number,
-}
+  name: string;
+  active: boolean;
+  value: number;
+};
