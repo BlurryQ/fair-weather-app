11033e5d3f266b2e2efcde54a9963eb4b402e749
BlurryQ <jazz.barlow@outlook.com>
2025-09-01 20:32:13 +0100

    Moves weather API files

--- src/components/LocationInput.tsx
+++ src/components/LocationInput.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import LocationList from './LocationList';
 
 // models
-import { getAutocompleteWeather } from '../../weatherAPI/weatherModel';
+import { getAutocompleteWeather } from '../models/weatherAPI/weatherModel';
 
 // types
 import { Autocomplete } from '../types/Autocomplete';
