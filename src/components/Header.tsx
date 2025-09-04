51af16b40b3f28b58aca465c84798034fcb2f0f3
BlurryQ <jazz.barlow@outlook.com>
2025-09-04 22:06:33 +0100

    Creates new props for search reset

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import '../styles/header.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // components
 import Location from './Location';
+import NavBar from './NavBar';
 
 // types
 import { WeatherDataProp } from '../types/WeatherDataProp';
-import NavBar from './NavBar';
-import { Link } from 'react-router-dom';
+import { Coords } from '../types/Coords';
+import { ResetSearch } from '../types/ResetSearch';
 
 export default function Header({
   setWeatherData,
@@ -15,17 +18,29 @@ export default function Header({
   setWeatherData: React.Dispatch<React.SetStateAction<WeatherDataProp | null>>;
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }): JSX.Element {
+  const [coords, setCoords] = useState<Coords>({ lon: 0, lat: 0 });
+
+  const resetSearch: ResetSearch = () => {
+    setWeatherData(null);
+    setCoords({ lon: 0, lat: 0 });
+  };
+
   return (
     <header>
       <div className="header">
-        <NavBar />
+        <NavBar resetSearch={resetSearch} />
         <h1>
-          <Link to="/" id="title">
+          <Link to="/" id="title" onClick={resetSearch}>
             Fair Weather App
           </Link>
         </h1>
       </div>
-      <Location setWeatherData={setWeatherData} setLoading={setLoading} />
+      <Location
+        setWeatherData={setWeatherData}
+        setLoading={setLoading}
+        coords={coords}
+        setCoords={setCoords}
+      />
     </header>
   );
 }
