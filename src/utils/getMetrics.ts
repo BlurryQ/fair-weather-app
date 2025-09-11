70b968b7d7687904083574d64214e448f40c5782
BlurryQ <jazz.barlow@outlook.com>
2025-09-11 21:57:51 +0100

    Creates new utility functions for settings cards

---
+++ src/utils/getMetrics.ts
@@ -0,0 +1,26 @@
+import { CoreSettings } from "../types/settings/CoreSettings";
+
+export default function getMetrics(settingName: string): string {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const coreSettings: CoreSettings = user.settings.coreSettings
+
+        switch (settingName) {
+      case 'Hot':
+      case 'Warm':
+      case 'Cold':
+        return coreSettings.is_celsius ? "°C" : "°F"
+      case 'rain_chance':
+      case 'snow_chance':
+        return '%';
+      case 'high_wind':
+      case 'low_wind':
+          case 'low_visability':
+        return coreSettings.is_miles ? "miles" : "kilometres"
+      case 'high_uv':
+        return 'index';
+      default:
+        return '';
+    }
+
+
+}
---
+++ src/utils/getSettingDetail.ts
@@ -0,0 +1,22 @@
+export default function getSettingDetail(settingName: string): string {
+    switch (settingName) {
+      case 'Hot':
+        return 'for temperatures above';
+      case 'Warm':
+        return 'for temperatures up to';
+      case 'Cold':
+        return 'for temperatures below';
+      case 'rain_chance':
+      case 'snow_chance':
+        return 'for chance above';
+      case 'high_wind':
+      case 'low_wind':
+        return 'for wind speeds above';
+      case 'high_uv':
+        return 'for uv above';
+      case 'low_visability':
+        return 'for visability below';
+      default:
+        return '';
+    }
+  }
